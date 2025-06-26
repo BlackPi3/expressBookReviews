a2f7180bff458ac5807cbc2dc809328f4a07ab74
Parham Zolfaghari <parhamzolfaqari@gmail.com>
2025-06-26 13:40:22 +0200

    fixed deleting reviews

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,7 +56,7 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 regd_users.delete("/auth/review/:isbn", (req, res) => {
   const isbn = req.params.isbn;
   const { review } = req.body;
-  const username = req.session.authorization.accessToken.username;
+  const username = req.username;
   if (books[isbn] && books[isbn].reviews[username]) {
     delete books[isbn].reviews[username];
     return res.status(200).json({ message: "Review deleted successfully", book: books[isbn] });
